c60061d21db5dea53d10cdae043eb162f9f58ce9
Ariel <bhe099@myy.haaga-helia.fi>
2024-04-19 10:31:55 +0300

    update vitconfig

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/PersonalTrainerCompany/',
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
 })
+
+
